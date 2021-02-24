1650ab4d5aae3336ef333fa6955a44828304ffc0
Micah Sterling <msterling89@gmail.com>
2021-02-24 15:15:15 -0600

    added single product of silverware

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -3,4 +3,9 @@ class Api::ProductsController < ApplicationController
     @products = Product.all
     render 'all_products.json.jb'
   end
+
+  def silverware
+    @product = Product.find_by(id: 1)
+    render 'silverware.json.jb'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
 
   namespace :api do
     get "/products" => "products#all_products"
+    get "/silverware" => "products#silverware"
   end
 end
