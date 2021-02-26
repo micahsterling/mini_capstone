7bbfcf8514491ff5c6d0641693805c2ef2ee99bc
Micah Sterling <msterling89@gmail.com>
2021-02-26 16:50:17 -0600

    hard coded product update

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -15,9 +15,17 @@ class Api::ProductsController < ApplicationController
     image_url: params[:image_url],
     description: params[:description],
     price: params[:price],
-    
     )
     @product.save
+    render 'show.json.jb'
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.name = "watering can"
+    @product.image_url = "url goes here"
+    @product.description = "used for watering plants"
+    @price = 20
     render 'show.json.jb'
   end
 end
