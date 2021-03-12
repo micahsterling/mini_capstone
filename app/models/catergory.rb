0a97a4bae5127fd7877b5600ce8f9c2c33dad78e
Micah Sterling <msterling89@gmail.com>
2021-03-11 18:41:30 -0600

    add catergory and catergory_product model with association

---
+++ app/models/catergory.rb
@@ -0,0 +1,3 @@
+class Catergory < ApplicationRecord
+has_many :category_products
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_11_020207) do
+ActiveRecord::Schema.define(version: 2021_03_12_003559) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "category_products", force: :cascade do |t|
+    t.integer "category_id"
+    t.integer "product_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "catergories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "images", force: :cascade do |t|
     t.string "url"
