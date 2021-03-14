51cd3dceed99d8b63cb90737e71a206f42e0f7fd
Micah Sterling <msterling89@gmail.com>
2021-03-14 11:36:45 -0500

    write associations

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,6 +9,9 @@ class Product < ApplicationRecord
   has_many :orders
   has_many :category_products
   has_many :categories, through: :category_products
+  has_many :carted_products
+  has_many :orders, through: :carted_products
+  has_many :users, throughL: :carted_products 
 
   def is_discounted?
     if price < 10
